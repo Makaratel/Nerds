e10554712ba6585c5b9849c08e1b1d40a20efad1
Makaratel <pmakaewskij@mail.ru>
2019-03-06 22:30:07 +0300

    Внесены изменения в рамках работы скрипта

--- js/script_catalog.js
+++ js/script_catalog.js
@@ -19,34 +19,36 @@ var resultMin = document.querySelector('.price-control__input--min');
 var resultMax = document.querySelector('.price-control__input--max');
 
 //Функция оценки координат ползунка
-var cordinates = function(scale, toogle, evt){
-	var scaleClientCoords = scale.getBoundingClientRect();
-	var toogleClientCoords = toogle.getBoundingClientRect();
-	var scaleCoords = {};
-	var toogleCoords = {};
+var cordinates = function(scale, toogleMin, toogleMax, evt){
+	var scaleCoords = scale.getBoundingClientRect();
+	var toogleMinCoords = toogleMin.getBoundingClientRect();
+	var toogleMaxCoords = toogleMax.getBoundingClientRect();
 
-	var scaleLeft = scaleClientCoords.left + pageXOffset;
-	var toogleLeft = toogleClientCoords.left + pageXOffset;
-	var availableRange = scale.offsetWidth - toogle.offsetWidth;
-	var shiftX = evt.pageX - toogleLeft;
+	var scaleLeft = scaleCoords.left + pageXOffset;
+	var scaleLength = scale.offsetWidth - toogleMin.offsetWidth;
+	var toogleMinLeft = toogleMinCoords.left + pageXOffset;
+	var toogleMaxLeft = toogleMaxCoords.left + pageXOffset;
+	var shift = evt.pageX - toogleMinLeft;
+	var rangePriceMin = toogleMaxLeft - scaleLeft - toogleMax.offsetWidth;
+	var rangePriceMax = toogleMaxLeft - scaleLeft + toogleMax.offsetWidth;
 
-	var massiv = [scaleLeft, toogleLeft, availableRange, shiftX];
+	var massiv = [scaleLeft, shift, rangePriceMin, rangePriceMax, scaleLength];
 	return massiv;
 }
 
 //Минимальная цена
 priceMin.onmousedown = function(evt){
-	var position = cordinates(slider, priceMin, evt);
+	var position = cordinates(slider, priceMin, priceMax, evt);
    
 	priceMin.ondragstart = function() {
     	return false;
 	};
 	document.onmousemove = function(evt){
-   		var newLeft = evt.pageX - position[0] - position[3];
+   		var newLeft = evt.pageX - position[0] - position[1];
     	if(newLeft < 0) newLeft = 0;
     	if(newLeft > position[2]) newLeft = position[2];
     	priceMin.style.left = newLeft + "px";
-    	var labelMin = Math.round(newLeft / position[2] * 15000);
+    	var labelMin = Math.round(newLeft / position[4] * 15000);
     	resultMin.setAttribute("value", labelMin);
     	bar.style.left = newLeft + "px";
 		return false;
@@ -58,17 +60,16 @@ priceMin.onmousedown = function(evt){
 
 //Максимальная цена
 priceMax.onmousedown = function(evt){
-	var position = cordinates(slider, priceMax, evt);
-   
+	var position = cordinates(slider, priceMax, priceMin, evt);
 	priceMax.ondragstart = function() {
     	return false;
 	};
 	document.onmousemove = function(evt){
-   		var newRight = evt.pageX - position[0] - position[3];
-    	if(newRight < 0) newLeft = 0;
-    	if(newRight > position[2]) newRight = position[2];
+   		var newRight = evt.pageX - position[0] - position[1];
+    	if(newRight < position[3]) newRight = position[3];
+    	if(newRight > position[4]) newRight = position[4];
     	priceMax.style.left = newRight + "px";
-    	var labelMax = Math.round(newRight / position[2] * 15000);
+    	var labelMax = Math.round(newRight / position[4] * 15000);
     	resultMax.setAttribute("value", labelMax);
     	bar.style.right = 210 - newRight + "px";
 		return false;
@@ -77,10 +78,3 @@ priceMax.onmousedown = function(evt){
    		document.onmousemove = null;
    }
 }
-
-resultMin.addEventListener("click", function(){
-	var position = cordinates(slider, priceMin, evt);
-	var valueMin = resultMin.getAttribute("value");
-	var newLeft = valueMin * position[2] / 15000;
-	priceMin.style.left = newLeft;
-})
